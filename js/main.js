0c97a26c7ed4c72554327ed620c872182baa2867
junki <junki646@gmail.com>
2022-07-21 17:25:38 +0900

    유령 이동, 다 떨어지면 죽음

--- js/main.js
+++ js/main.js
@@ -1,22 +1,37 @@
-const img = document.querySelector('#hero')
+const hero = document.querySelector('#hero')
+const enemy = document.querySelector('#enemy')
 
 window.addEventListener("keydown", (e) => {
     let x=0;
+    let y=0;
 
-    if (e.key === 'ArrowLeft' && img.offsetLeft>20) {
-        x = img.offsetLeft - 10;
-        img.style.left = `${x}px`;   
-        img.className='heroLeft'       
+    if (e.key === 'ArrowLeft' && hero.offsetLeft>20) {
+        x = hero.offsetLeft - 10;
+        hero.style.left = `${x}px`;   
+        hero.className='heroLeft'       
     }
 
-    if (e.key === 'ArrowRight' && img.offsetLeft<780) {
-        let x = img.offsetLeft + 10;
-        img.style.left = `${x}px`;
-        img.className='heroRight'
+    if (e.key === 'ArrowRight' && hero.offsetLeft<780) {
+        x = hero.offsetLeft + 10;
+        hero.style.left = `${x}px`;
+        hero.className='heroRight'
     }
 });
 
 window.addEventListener("keyup", (e)=>{
-    img.className='heroStop'
-    console.log(img)
+    hero.className='heroStop'
+    console.log(hero)
 });
+
+function enemyMove() {
+    if(enemy.offsetTop>540){
+        enemy.className='enemyDeath'
+        return clearInterval()
+    }
+    y = enemy.offsetTop + 10;
+    enemy.style.top = `${y}px`;   
+}
+
+setInterval(()=>{
+    enemyMove()
+},40)
